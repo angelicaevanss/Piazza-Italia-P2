f0aa6696b9bd0f3a88346a784791e1319928eb52
Angelica <angelica.evans@shaw.ca>
2019-11-25 17:29:11 -0800

    idek

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,32 @@
 $(function() {
 // Your Code from here on down. Don't delete that line above!
   
+'use strict'
+
+  function toggleIcon () {
+  	$('.icon').on('click', function(){
+  		$('.icon').toggleClass('active');
+  		$('.menu-mobile').slideToggle(300);
+  	})
+  }
+
+    function closeMobileMenu (){
+  		$('.menu-mobile').on('click', 'a', function(){
+  			$('.icon').trigger('click');
+  		})
+  }
+
+  function desktopMenu() {
+  		$(window).scroll(function(){
+  			if ($(this).scrollTop() > 50 && $(window).width() >
+  				$('header.desktop').fadeIn(500);
+  			} else {
+  				$('header.desktop').fadeOut(500);
+  			}
+  		})
+  }
+
+
 
 
 
