e1ffbdc9490d0b1bae027bda9803fd527446c162
Angelica <angelica.evans@shaw.ca>
2019-12-02 14:54:35 -0800

    JQUERY NOT WORKING??

--- scripts/script.js
+++ scripts/script.js
@@ -2,42 +2,11 @@
 $(function() {
 // Your Code from here on down. Don't delete that line above!
   
-'use strict'
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+$('.logo').click(function() {
+	$('.logo').css('display', 'none;')
+})
 
 
-function hover() {
-  $(".button").on("mouseenter", function() {
-    return $(this).addClass("hover");
-  });
-}
-
-function hoverOff() {
-  $(".button").on("mouseleave", function() {
-    return $(this).removeClass("hover");
-  });
-}
-
-function active() {
-  $(".button").on("click", function() {
-    return $(this).addClass("active");
-  });
-}
-
-hover();
-hoverOff();
-active();
 
 
 
