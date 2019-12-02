02f0246518e99dce4c5bbe98a7f68d824fabe593
Angelica <angelica.evans@shaw.ca>
2019-12-01 20:45:32 -0800

    i made it sticky but its sticking to the top and not below the header

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,19 @@ $(function() {
 // Your Code from here on down. Don't delete that line above!
   
 'use strict'
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
 
 
 
